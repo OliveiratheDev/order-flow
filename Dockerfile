2aac27e35922b8a350b65ce640aa1c5916c2c092
Oliveira dev <gabrieloliveira362978@gmail.com>
2026-07-28 23:01:32 -0300

    feat: configuracao de ambiente de desenvolvimento

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM ubuntu:latest
+LABEL authors="Admin"
+
+ENTRYPOINT ["top", "-b"]
+
+ENV
---
+++ src/test/java/com/start/overflow/OverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.start.overflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
